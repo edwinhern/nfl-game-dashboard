a7df03a70d850b2cc4e1dc2b652439d51de2d893
Edwin Hernandez <edwinhern.16@gmail.com>
2024-10-16 20:12:31 -0500

    ci: :green_heart: Update env to contain testOnly variables

--- backend/src/env.ts
+++ backend/src/env.ts
@@ -1,17 +1,18 @@
 import dotenv from "dotenv";
-import { bool, cleanEnv, host, port, str } from "envalid";
+import { bool, cleanEnv, host, port, str, testOnly } from "envalid";
 
 dotenv.config();
 
 export default cleanEnv(process.env, {
 	NODE_ENV: str({
+		devDefault: testOnly("test"),
 		desc: "The environment the server is running in",
 		choices: ["development", "production", "test"],
 	}),
-	HOST: host({ desc: "The host to start the server on" }),
-	PORT: port({ desc: "The port to start the server on" }),
-	DATABASE_URL: str({ desc: "The URL to the database" }),
-	TICKETMASTER_API_KEY: str({ desc: "The API key for the Ticketmaster API" }),
-	SYNC_SCHEDULE: str({ desc: "The cron schedule for syncing games" }),
-	DEBUG_MODE: bool({ desc: "Whether to enable debug mode", default: false }),
+	HOST: host({ devDefault: testOnly("localhost"), desc: "The host to start the server on" }),
+	PORT: port({ devDefault: testOnly(3000), desc: "The port to start the server on" }),
+	DATABASE_URL: str({ devDefault: testOnly(""), desc: "The URL to the database" }),
+	TICKETMASTER_API_KEY: str({ devDefault: testOnly(""), desc: "The API key for the Ticketmaster API" }),
+	SYNC_SCHEDULE: str({ devDefault: testOnly("0 */12 * * *"), desc: "The cron schedule for syncing games" }),
+	DEBUG_MODE: bool({ devDefault: testOnly(false), desc: "Whether to enable debug mode" }),
 });
